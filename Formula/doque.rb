12220f54f1e2c1614ada91f683e34ace236f830c
Yuriy Sagitov <pro100.ko10ok@gmail.com>
2025-11-26 16:24:39 +0300

    add README, requirements, and update script for Homebrew tap

--- Formula/doque.rb
+++ Formula/doque.rb
@@ -3,8 +3,8 @@ class Doq < Formula
 
   desc "Do Query - cli llm client"
   homepage "https://github.com/ko10ok/do"
-  url "https://files.pythonhosted.org/packages/4e/a9/4e81c9d78a3e8b555ed4cfc3a2d944fde9f7af4e26384e10ba29bb802ab9/doque-0.0.1.tar.gz"
-  sha256 "4db6b09d3b1bc259edca5459f6d2c465908fc72cad4ad7d4b64d26bceb601a8b"
+  url "https://files.pythonhosted.org/packages/c1/3b/cd4dbe57e6c4a2562e61a896c4f97102a79565253c4a79b8e43ed7a9d65d/doque-0.2.0.tar.gz"
+  sha256 "8ce2e9a3e1f97cca1d4cd9c446d7f3fdb206baf679cd02b848afc661189ecf8e"
   license "Apache-2.0"
 
   depends_on "python@3.12"
